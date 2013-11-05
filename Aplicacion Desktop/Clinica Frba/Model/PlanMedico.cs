e940a9d0e3371f00fd91653aee4895e784204d63
muehara <maxi.uehara@gmail.com>
2013-11-05 11:15:13 -0300

    Model. No se si respeta ADO pero we salio asi. La idea es tener un Clase bean que contiene, ademas de las propiedades, un obejto repositri que es de clase. Que es donde va estar todas los accesos a bd

---
+++ Aplicacion Desktop/Clinica Frba/Model/PlanMedico.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Clinica_Frba.Model.Repository;
+
+namespace Clinica_Frba.Model
+{
+    public class PlanMedico
+    {
+        private String codigo;
+        private String descripcion;
+        private float precioBonoFarmacia;
+        private float precioBonoConsulta;
+        private Boolean enable;
+        private Boolean isHistorico;
+        private static PlanMedicoRepository repository = new PlanMedicoRepository();
+
+        public String Codigo
+        {
+            set { codigo = value; }
+            get { return codigo; }
+        }
+        public String Descripcion
+        {
+            set { descripcion = value; }
+            get { return descripcion; }
+        }
+        public float PrecioBonoFarmacia
+        {
+            set { precioBonoFarmacia = value; }
+            get { return precioBonoFarmacia; }
+        }
+        public float PrecioBonoConsulta
+        {
+            set { precioBonoConsulta = value; }
+            get { return precioBonoConsulta;}
+        }
+        public Boolean Enable
+        {
+            set { enable = value; }
+            get { return enable; }
+        }
+        public Boolean IsHistorico
+        {
+            set { isHistorico = value; }
+            get { return isHistorico; }
+        }
+        public static PlanMedicoRepository getRepository
+        {
+            get { return repository; }
+        }
+
+    }
+}
---
+++ Aplicacion Desktop/Clinica Frba/Model/Repository/Repository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Clinica_Frba.Model.Repository
+{
+    public class Repository
+    {
+        public Repository()
+        {
+        }
+
+        public DataTable listar(String query)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Properties.Settings.Default.dbConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                try
+                {
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); 
+                    DataTable dataTable = new DataTable();
+                    sqlDataAdapter.Fill(dataTable);
+                    return (dataTable);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
+                    return null;
+                }
+                finally
+                {
+                    sqlConnection.Dispose();
+                    sqlCommand.Dispose();
+                }
+            }
+            
+            
+        }
+        public DataTable listar(String spName, List<SqlParameter> parametros)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Properties.Settings.Default.dbConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(spName, sqlConnection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddRange(parametros.ToArray());
+                try
+                {
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                    DataTable dataTable = new DataTable();
+                    sqlDataAdapter.Fill(dataTable);
+                    return (dataTable);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
+                    return null;
+                }
+                finally
+                {
+                    sqlConnection.Dispose();
+                    sqlCommand.Dispose();
+                }
+            }
+
+
+        }
+   }
+}
