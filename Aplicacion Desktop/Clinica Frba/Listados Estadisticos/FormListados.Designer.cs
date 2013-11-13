51e24c09a4e7d77bda22d84c64e832a3611e4dfa
muehara <maxi.uehara@gmail.com>
2013-11-13 09:05:09 -0300

    PUNTO 15 LISTADOS FALTAN LOS ESTORE PROCEDURE NO MAS

---
+++ Aplicacion Desktop/Clinica Frba/Listados Estadisticos/FormListados.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Clinica_Frba.NewFolder9
+{
+    partial class FormListados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbSelector = new System.Windows.Forms.GroupBox();
+            this.btGenerar = new System.Windows.Forms.Button();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.cbPeriodo = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbYear = new System.Windows.Forms.ComboBox();
+            this.lA = new System.Windows.Forms.Label();
+            this.lTipo = new System.Windows.Forms.Label();
+            this.cbReportes = new System.Windows.Forms.ComboBox();
+            this.gbSelector.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbSelector
+            // 
+            this.gbSelector.Controls.Add(this.btGenerar);
+            this.gbSelector.Controls.Add(this.dgvLista);
+            this.gbSelector.Controls.Add(this.lbTitulo);
+            this.gbSelector.Controls.Add(this.cbPeriodo);
+            this.gbSelector.Controls.Add(this.label3);
+            this.gbSelector.Controls.Add(this.cbYear);
+            this.gbSelector.Controls.Add(this.lA);
+            this.gbSelector.Controls.Add(this.lTipo);
+            this.gbSelector.Controls.Add(this.cbReportes);
+            this.gbSelector.Location = new System.Drawing.Point(12, 12);
+            this.gbSelector.Name = "gbSelector";
+            this.gbSelector.Size = new System.Drawing.Size(795, 394);
+            this.gbSelector.TabIndex = 0;
+            this.gbSelector.TabStop = false;
+            this.gbSelector.Text = "Opciones";
+            // 
+            // btGenerar
+            // 
+            this.btGenerar.Location = new System.Drawing.Point(609, 102);
+            this.btGenerar.Name = "btGenerar";
+            this.btGenerar.Size = new System.Drawing.Size(157, 23);
+            this.btGenerar.TabIndex = 15;
+            this.btGenerar.Text = "Listar";
+            this.btGenerar.UseVisualStyleBackColor = true;
+            this.btGenerar.Click += new System.EventHandler(this.btGenerar_Click);
+            // 
+            // dgvLista
+            // 
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.Location = new System.Drawing.Point(18, 144);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.Size = new System.Drawing.Size(748, 229);
+            this.dgvLista.TabIndex = 14;
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Location = new System.Drawing.Point(15, 16);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(0, 13);
+            this.lbTitulo.TabIndex = 13;
+            // 
+            // cbPeriodo
+            // 
+            this.cbPeriodo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPeriodo.FormattingEnabled = true;
+            this.cbPeriodo.Location = new System.Drawing.Point(135, 102);
+            this.cbPeriodo.Name = "cbPeriodo";
+            this.cbPeriodo.Size = new System.Drawing.Size(256, 21);
+            this.cbPeriodo.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "EL TRIMESTRE";
+            // 
+            // cbYear
+            // 
+            this.cbYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbYear.FormattingEnabled = true;
+            this.cbYear.Location = new System.Drawing.Point(137, 69);
+            this.cbYear.Name = "cbYear";
+            this.cbYear.Size = new System.Drawing.Size(254, 21);
+            this.cbYear.TabIndex = 10;
+            // 
+            // lA
+            // 
+            this.lA.AutoSize = true;
+            this.lA.Location = new System.Drawing.Point(15, 69);
+            this.lA.Name = "lA";
+            this.lA.Size = new System.Drawing.Size(116, 13);
+            this.lA.TabIndex = 9;
+            this.lA.Text = "SELECCIONE EL AÑO";
+            // 
+            // lTipo
+            // 
+            this.lTipo.AutoSize = true;
+            this.lTipo.Location = new System.Drawing.Point(15, 31);
+            this.lTipo.Name = "lTipo";
+            this.lTipo.Size = new System.Drawing.Size(99, 13);
+            this.lTipo.TabIndex = 8;
+            this.lTipo.Text = "TIPO DE LISTADO";
+            // 
+            // cbReportes
+            // 
+            this.cbReportes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbReportes.FormattingEnabled = true;
+            this.cbReportes.Location = new System.Drawing.Point(135, 28);
+            this.cbReportes.Name = "cbReportes";
+            this.cbReportes.Size = new System.Drawing.Size(256, 21);
+            this.cbReportes.TabIndex = 7;
+            // 
+            // FormListados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 418);
+            this.Controls.Add(this.gbSelector);
+            this.Name = "FormListados";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormListados_Load);
+            this.gbSelector.ResumeLayout(false);
+            this.gbSelector.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbSelector;
+        private System.Windows.Forms.ComboBox cbPeriodo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbYear;
+        private System.Windows.Forms.Label lA;
+        private System.Windows.Forms.Label lTipo;
+        private System.Windows.Forms.ComboBox cbReportes;
+        private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.DataGridView dgvLista;
+        private System.Windows.Forms.Button btGenerar;
+    }
+}
---
+++ Aplicacion Desktop/Clinica Frba/Listados Estadisticos/FormListados.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Clinica_Frba.Base;
+using System.Data.SqlClient;
+using Clinica_Frba.Model.Repository;
+
+namespace Clinica_Frba.NewFolder9
+{
+    public partial class FormListados : FormBase
+    {
+        public FormListados()
+        {
+            InitializeComponent();
+        }
+        private Dictionary<int, delegateListados> listados = new Dictionary<int, delegateListados>();
+
+        delegate void delegateListados(DateTime fechaF0, DateTime fechaF1);
+
+        private void ini_mapListados()
+        {
+            listados.Add(0, new delegateListados(this.listaTop5CancelacionesByEspecialidades));
+            listados.Add(1, new delegateListados(this.listaTop5BonosFarmacia));
+            listados.Add(2, new delegateListados(this.listaTop5EspecialidadesByMedicosWithMoreBonos));
+            listados.Add(3, new delegateListados(this.listaTop10AfiliadosByBonosNoCompradosPorEllos));
+        }
+
+        private void ini_comboReportes()
+        {
+            List<string> listaReportes = new List<string>();
+            listaReportes.Add("Top  5  de  las  especialidades  que  más  se  registraron  cancelaciones,  tanto  de pacientes como de profesionales.");
+            listaReportes.Add("Top 5 de la cantidad total de bonos farmacia vencidos por afiliado.");
+            listaReportes.Add("Top 5 de las especialidades de médicos con más bonos de farmacia recetados.");
+            listaReportes.Add("Top 10 de los afiliados que utilizaron bonos que ellos mismo no compraron.");
+            cbReportes.DataSource = listaReportes;
+        }
+
+        private void ini_comboSemetre()
+        {
+            List<string> lista = new List<string>();
+            lista.Add("Primer semestre (Enero - Junio)");
+            lista.Add("Segundo trimestre (Julio - Diciembre)");
+            cbPeriodo.DataSource = lista;
+        }
+
+        private void ini_comboYear()
+        {
+            List<int> lista = new List<int>();
+            DateTime now;
+
+            // Tomo la fecha definida en el config. Si esta fecha esta 
+            // Mal definda se toma la fecha actual
+            now = getFechaConfig();
+            
+            for (int i = 0; i < (now.Year - Properties.Settings.Default.yearF0) + 1; i++)
+            {
+                lista.Add(Properties.Settings.Default.yearF0 + i);
+            }
+            cbYear.DataSource = lista;
+        }
+
+        private void FormListados_Load(object sender, EventArgs e)
+        {
+            this.ini_comboReportes();
+            this.ini_comboYear();
+            this.ini_comboSemetre();
+            this.ini_mapListados();
+            this.cbPeriodo.SelectedIndex = 0;
+            this.cbReportes.SelectedIndex = 0;
+            this.cbYear.SelectedIndex = 0;
+        }
+
+        private void ejecutar(String sql, DateTime fechaF0, DateTime fechaF1)
+        {
+            List<SqlParameter> lst = new List<SqlParameter>();
+            SqlParameter param = new SqlParameter("fechaF0", SqlDbType.DateTime);
+            param.Value = fechaF0;
+            lst.Add(param);
+            param = new SqlParameter("fechaF1", SqlDbType.DateTime);
+            param.Value = fechaF1;
+            lst.Add(param);
+
+            DataTable dt = (new Repository()).listar(sql, lst);
+            this.dgvLista.DataSource = dt;
+        }
+
+        // Sobreprograme un poco. Con una lista de nombres de storeProcedure puede que sea mas simple
+        // Top  5  de  las  especialidades  que  más  se  registraron  cancelaciones,  tanto  de pacientes como de profesionales.
+        private void listaTop5CancelacionesByEspecialidades(DateTime fechaF0, DateTime fechaF1)
+        {
+            lbTitulo.Text = "Top  5  de  las  especialidades  que  más  se  registraron  cancelaciones,  tanto  de pacientes como de profesionales.";
+            this.ejecutar("[NN_NN].[SP_TOP_5_CANCELACIONES_BY_ESPECIALIDADES]", fechaF0, fechaF1);
+        }
+
+        //Top 5 de la cantidad total de bonos farmacia vencidos por afiliado.
+        private void listaTop5BonosFarmacia(DateTime fechaF0, DateTime fechaF1)
+        {
+            lbTitulo.Text = "Top 5 de la cantidad total de bonos farmacia vencidos por afiliado.";
+            this.ejecutar("[NN_NN].[SP_TOP_5_BONOS_FARMACIA]", fechaF0, fechaF1);
+        }
+        //Top 5 de las especialidades de médicos con más bonos de farmacia recetados.
+        private void listaTop5EspecialidadesByMedicosWithMoreBonos(DateTime fechaF0, DateTime fechaF1)
+        {
+            lbTitulo.Text = "Top 5 de las especialidades de médicos con más bonos de farmacia recetados.";
+            this.ejecutar("[NN_NN].[SP_TOP_5_ESPECIALIDADES_BY_MORE_BONOS]", fechaF0, fechaF1);
+        }
+
+        //Top 10 de los afiliados que utilizaron bonos que ellos mismo no compraron.
+        private void listaTop10AfiliadosByBonosNoCompradosPorEllos(DateTime fechaF0, DateTime fechaF1)
+        {
+            lbTitulo.Text = "Top 10 de los afiliados que utilizaron bonos que ellos mismo no compraron.";
+            this.ejecutar("[NN_NN].[SP_TOP_10_AFILIADOS_BONOS_COMPRADOS_POR_OTROS]", fechaF0, fechaF1);
+        }
+
+        private void btGenerar_Click(object sender, EventArgs e)
+        {
+            int year = (int)cbYear.SelectedItem;
+            int monthF0 = (cbPeriodo.SelectedIndex * 6) + 1;
+            int monthF1 = (cbPeriodo.SelectedIndex * 6) + 6;
+            DateTime fechaF0 = new DateTime(year, monthF0, 1);
+            DateTime fechaF1 = new DateTime(year, monthF1, 1);
+            fechaF1 = fechaF1.AddMonths(1).AddDays(-1).AddHours(23).AddMinutes(59).AddSeconds(59);
+            delegateListados delegado = listados[cbReportes.SelectedIndex];
+            delegado(fechaF0, fechaF1);
+        }
+    }
+}
