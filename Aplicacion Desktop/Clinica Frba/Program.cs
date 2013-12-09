0ec21aef4a089bf7c89408a4cf4196ac7e94dbfc
durden <llrbreuer@gmail.com>
2013-12-09 16:41:02 -0300

    listados

--- Aplicacion Desktop/Clinica Frba/Program.cs
+++ Aplicacion Desktop/Clinica Frba/Program.cs
@@ -19,23 +19,14 @@ namespace Clinica_Frba
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             
-            /*
+            
             FormLogin frmLogin = new FormLogin();
             if (frmLogin.ShowDialog() == DialogResult.OK)
             {
                 Application.Run(new FormMain());
-            }*/
+            }
 
-            DataSession.idRol = 1;
-            DataSession.nroAfiliado = "7149";
-            DataSession.nroDiscriminadorAfiliado = "1";
-            DataSession.nroAfiliadoReceta = 3;
-            DataSession.nroConsulta = 108318;
-            DataSession.profesionalSession = new Profesional(1, "apellido", "nombre", "email", 1);
-            DataSession.afiliadoSession = new Afiliado();
-            DataSession.afiliadoSession.NroAfiliado = 7434;
-            DataSession.afiliadoSession.NroDiscriminador = 1;
-            Application.Run(new FormMain());
+       
   
         }
     }
